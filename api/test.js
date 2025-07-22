52e72e28d6d5ca5fd3e70ceb512976ead8993d78
Surwix <surwixsupply@gmail.com>
2025-07-21 22:08:24 -0400

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,1 +1,4 @@
-
+export default function handler(req, res) {
+  console.log(`[${new Date().toISOString()}] Test function invoked successfully.`);
+  res.status(200).json({ status: 'ok', message: 'Hello from the API!' });
+}
